5be1d1ea771d7277bd0767ba44ef15ce36e0edc8
Joaquin Arcilla <joaquin.arcilla@gmail.com>
2025-01-21 20:01:30 -0500

    Modified tips page

--- lib/main.dart
+++ lib/main.dart
@@ -28,21 +28,24 @@ class TipsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('First Route'),
+
       ),
       body: Center(
         child: Column(
           children: [
             const Image(image: AssetImage('assets/logo.png'), width:50),
-            const Text('1.  For the best results, keep your\n hands still or rest your phone on\n something to avoid blurry\n pictures.\n2.  Try to avoid shadows and\n  reflections.\n3.  Tap on screen to focus\n  properly.'),
+            const Text('1.  For the best results, keep your\n     hands still or rest your phone on\n     something to avoid blurry\n     pictures.\n2.  Try to avoid shadows and\n     reflections.\n3.  Tap on screen to focus\n     properly.'),
             ElevatedButton(
-              child: const Text('Next'),
               onPressed: () {
                 Navigator.push(
                   context,
                   MaterialPageRoute(builder: (context) => const SamplePage()),
                 );
               },
+              style: ElevatedButton.styleFrom(
+              backgroundColor: const Color.fromARGB(255, 0, 127, 163), // background
+              ),
+              child: const Text('Next', style: TextStyle(color: Colors.white,),), 
             ),
           ],
         ),
@@ -58,7 +61,7 @@ class SamplePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Second Route'),
+
       ),
       body: Center(
         child: ElevatedButton(
