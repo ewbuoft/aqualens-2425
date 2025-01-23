7634a008cee9fbb9a966de1fda52da42ff4a6084
aaliyahpirani <138330227+aaliyahpirani@users.noreply.github.com>
2025-01-23 11:24:01 -0500

    added functional button that goes to homepage

--- lib/main.dart
+++ lib/main.dart
@@ -5,20 +5,24 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Login Page',
       theme: ThemeData(
-        primaryColor: Color.fromRGBO(0, 127, 163, 1.0), // Pantone 633
+        primaryColor: const Color.fromRGBO(0, 127, 163, 1.0), // Pantone 633
       ),
-      home: HomePage(),
+      home: LoginPage(),
       debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class HomePage extends StatelessWidget {
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,16 +36,73 @@ class HomePage extends StatelessWidget {
               child: Image.asset('assets/logo_with_text.png'),
             ),
           ),
-          Center(
+
+          Positioned(
+            top: 360,
+            left: MediaQuery.of(context).size.width / 2 - 90,
             child: Text(
-              'Insert Unique ID',
-              style: TextStyle(fontSize: 18,
-              fontFamily: 'verdana',
-              color: Theme.of(context).primaryColor),
+              'Put Your Unique ID Here: ',
+              style: TextStyle(
+              fontSize: 18,
+              fontFamily: 'Verdana',
+              color: Theme.of(context).primaryColor, 
+              ),
+          ),
+      ),     
+         const Align(
+            alignment: Alignment.center,
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 32), 
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Unique ID',
+                  border: OutlineInputBorder(),
+                ),
+              ),
             ),
           ),
+
+          Align(
+            alignment: Alignment.center,
+            child: Padding(
+              padding: const EdgeInsets.only(top: 140),
+              
+            child: ElevatedButton( 
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => const HomePage())
+                );
+              },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Theme.of(context).primaryColor,
+              foregroundColor: Colors.white,
+              padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 12),
+             ),
+              child: const Text('Enter'
+                 ),
+              ),
+
+            ),
+          
+          
+          )
         ],
       ),
     );
+  }
+}
+
+// Sample Home Page for Now
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: const Center(
+        child: Text('Successful Login !!'),
+        ),
+      );
   }
 }
