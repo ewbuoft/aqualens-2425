4d43cae44d247d52f8971772e6e3a8430eacf6cc
Joaquin Arcilla <joaquin.arcilla@gmail.com>
2025-01-28 19:43:46 -0500

    Added more pages. TODO: Add actual relevant images and diagrams

--- lib/main.dart
+++ lib/main.dart
@@ -35,11 +35,12 @@ class TipsPage extends StatelessWidget {
           children: [
             const Image(image: AssetImage('assets/logo.png'), width:50),
             const Text('1.  For the best results, keep your\n     hands still or rest your phone on\n     something to avoid blurry\n     pictures.\n2.  Try to avoid shadows and\n     reflections.\n3.  Tap on screen to focus\n     properly.'),
+            const Spacer(flex:2),
             ElevatedButton(
               onPressed: () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => const SamplePage()),
+                  MaterialPageRoute(builder: (context) => const WaterSamplingSourceA()),
                 );
               },
               style: ElevatedButton.styleFrom(
@@ -47,6 +48,411 @@ class TipsPage extends StatelessWidget {
               ),
               child: const Text('Next', style: TextStyle(color: Colors.white,),), 
             ),
+            const Spacer(flex:1),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class WaterSamplingSourceA extends StatelessWidget {
+  const WaterSamplingSourceA({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const Text("Capture an image of the water sampling\nsource. Please ensure the entire tank or\nflush diverter and its surrounding\nenvironment are visible. Frame the scene\nto include both the system and its setting."),
+            const Spacer(),
+            const Image(image: AssetImage('assets/image_placeholder.jpg'), width:300),
+            const Spacer(),
+            ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const WaterSourcePhotoPlaceHolder()),
+                );
+            },
+            style: ElevatedButton.styleFrom(
+            backgroundColor: const Color.fromARGB(255, 0, 127, 163), // background
+            ),
+            child: const Text('Capture Image', style: TextStyle(color: Colors.white,),),
+            ),
+            const Spacer(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class WaterSamplingSourceB extends StatelessWidget {
+  const WaterSamplingSourceB({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const Text("Are you happy with the capture, or would\nyou like to retake the image?"),
+            const Spacer(),
+            const Image(image: AssetImage('assets/image_placeholder.jpg'), width:300),
+            const Spacer(),
+            Row(children: [
+              const Spacer(flex: 2),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => const WaterTapA()),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                backgroundColor: const Color.fromARGB(255, 0, 127, 163), // background
+                ),
+                child: const Text('Happy!', style: TextStyle(color: Colors.white,),),
+              ),
+              const Spacer(),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => const WaterSamplingSourceA()),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                backgroundColor: const Color.fromARGB(255, 0, 127, 163), // background
+                ),
+                child: const Text('Retake Image!', style: TextStyle(color: Colors.white,),),
+              ),
+              const Spacer(flex: 2),
+            ],),
+            const Spacer(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class WaterSourcePhotoPlaceHolder extends StatelessWidget {
+  const WaterSourcePhotoPlaceHolder({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+
+      ),
+      body: Center(
+        child:Column (
+          children: [
+            const Text('Insert Photo Taking Page Here!'),
+            const Spacer(),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const WaterSamplingSourceB()),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+              backgroundColor: const Color.fromARGB(255, 0, 127, 163), // background
+              ),
+              child: const Text('Continue', style: TextStyle(color: Colors.white,),),
+            ),
+            const Spacer(flex: 2),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class WaterTapA extends StatelessWidget {
+  const WaterTapA({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const Text("Please capture a clear image of the water\n tap. Focus closely so that the tap and any\nconnecting fixtures are clearly visible."),
+            const Spacer(),
+            const Image(image: AssetImage('assets/image_placeholder.jpg'), width:300),
+            const Spacer(),
+            ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const WaterTapPhotoPlaceHolder()),
+                );
+            },
+            style: ElevatedButton.styleFrom(
+            backgroundColor: const Color.fromARGB(255, 0, 127, 163), // background
+            ),
+            child: const Text('Capture Image', style: TextStyle(color: Colors.white,),),
+            ),
+            const Spacer(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class WaterTapB extends StatelessWidget {
+  const WaterTapB({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const Text("AIs this image clear and focused? If not\nhappy, please retake."),
+            const Spacer(),
+            const Image(image: AssetImage('assets/image_placeholder.jpg'), width:300),
+            const Spacer(),
+            Row(children: [
+              const Spacer(flex: 2),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => const RCardTips()),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                backgroundColor: const Color.fromARGB(255, 0, 127, 163), // background
+                ),
+                child: const Text('Happy!', style: TextStyle(color: Colors.white,),),
+              ),
+              const Spacer(),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => const WaterTapA()),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                backgroundColor: const Color.fromARGB(255, 0, 127, 163), // background
+                ),
+                child: const Text('Retake Image!', style: TextStyle(color: Colors.white,),),
+              ),
+              const Spacer(flex: 2),
+            ],),
+            const Spacer(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class WaterTapPhotoPlaceHolder extends StatelessWidget {
+  const WaterTapPhotoPlaceHolder({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+
+      ),
+      body: Center(
+        child:Column (
+          children: [
+            const Text('Insert Photo Taking Page Here!'),
+            const Spacer(),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const WaterTapB()),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+              backgroundColor: const Color.fromARGB(255, 0, 127, 163), // background
+              ),
+              child: const Text('Continue', style: TextStyle(color: Colors.white,),),
+            ),
+            const Spacer(flex: 2),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class RCardTips extends StatelessWidget {
+  const RCardTips({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const Image(image: AssetImage('assets/logo.png'), width:50),
+            const Text(''),
+            const Spacer(flex:2),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const RCardA()),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+              backgroundColor: const Color.fromARGB(255, 0, 127, 163), // background
+              ),
+              child: const Text('Next', style: TextStyle(color: Colors.white,),), 
+            ),
+            const Spacer(flex:1),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class RCardA extends StatelessWidget {
+  const RCardA({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const Text("Please take a photo of the R-card after\n24 hours. Make sure the card is in a\nwell-lit area, the grid area is centered\nand fitting in the box, and clearly visible\nfrom the top."),
+            const Spacer(),
+            const Image(image: AssetImage('assets/image_placeholder.jpg'), width:300),
+            const Spacer(),
+            ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const RCardPhotoPlaceholder()),
+                );
+            },
+            style: ElevatedButton.styleFrom(
+            backgroundColor: const Color.fromARGB(255, 0, 127, 163), // background
+            ),
+            child: const Text('Capture Image', style: TextStyle(color: Colors.white,),),
+            ),
+            const Spacer(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class RCardB extends StatelessWidget {
+  const RCardB({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const Text("Is this image clear and focused? If not\nhappy, please retake. Otherwise, process\nresults."),
+            const Spacer(),
+            const Image(image: AssetImage('assets/image_placeholder.jpg'), width:300),
+            const Spacer(),
+            Row(children: [
+              const Spacer(flex: 2),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => const RCardTips()),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                backgroundColor: const Color.fromARGB(255, 0, 127, 163), // background
+                ),
+                child: const Text('Happy!', style: TextStyle(color: Colors.white,),),
+              ),
+              const Spacer(),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => const RCardA()),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                backgroundColor: const Color.fromARGB(255, 0, 127, 163), // background
+                ),
+                child: const Text('Retake Image!', style: TextStyle(color: Colors.white,),),
+              ),
+              const Spacer(flex: 2),
+            ],),
+            const Spacer(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class RCardPhotoPlaceholder extends StatelessWidget {
+  const RCardPhotoPlaceholder({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+
+      ),
+      body: Center(
+        child:Column (
+          children: [
+            const Text('Insert Photo Taking Page Here!'),
+            const Spacer(),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const RCardB()),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+              backgroundColor: const Color.fromARGB(255, 0, 127, 163), // background
+              ),
+              child: const Text('Continue', style: TextStyle(color: Colors.white,),),
+            ),
+            const Spacer(flex: 2),
           ],
         ),
       ),
@@ -63,14 +469,7 @@ class SamplePage extends StatelessWidget {
       appBar: AppBar(
 
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: const Text('Go back!'),
-        ),
-      ),
+      body: Center(),
     );
   }
 }
