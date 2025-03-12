5b8e61ea89167a3b1e4716e28a4ef556e04465d1
alexandma <maddyalexander123@gmail.com>
2025-03-12 03:02:40 -0400

    Homepage fixes, welcome page, integration

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,14 @@ import 'login_and_signup.dart';
 void main() {
   runApp(const MyApp());
 }
-
+/*
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Login Page',
+      title: 'CGEN LluviaLens',
       theme: ThemeData(
         primaryColor: const Color.fromRGBO(0, 127, 163, 1.0), // Pantone 633
       ),
@@ -21,3 +21,46 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+*/
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CGEN Aqualens',
+      theme: ThemeData(
+          useMaterial3: true,
+
+          colorScheme: const ColorScheme (
+              brightness: Brightness.light,
+              primary:  Color.fromRGBO(0, 127, 163, 1.0),
+              onPrimary: Colors.white,
+              secondary: Color.fromRGBO(111, 199, 234, 1.0),
+              onSecondary: Colors.black,
+              error: Color.fromRGBO(220, 70, 51, 1.0),
+              onError: Colors.white,
+              surface: Colors.white,
+              onSurface: Colors.black)
+      ),
+      darkTheme: ThemeData(
+          useMaterial3: true,
+
+          colorScheme: const ColorScheme (
+              brightness: Brightness.light,
+              primary:  Color.fromRGBO(0, 127, 163, 1.0),
+              onPrimary: Colors.white,
+              secondary: Color.fromRGBO(111, 199, 234, 1.0),
+              onSecondary: Colors.black,
+              error: Color.fromRGBO(220, 70, 51, 1.0),
+              onError: Colors.white,
+              surface: Colors.black,
+              onSurface: Colors.white)
+      ),
+      home: const SwipePages(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
